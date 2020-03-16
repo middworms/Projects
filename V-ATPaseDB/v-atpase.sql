de7c2abf917243952dd6446d86344a9b46309a46
middworms <middworms@gmail.com>
2020-03-16 00:43:00 -0400

    the start of instructions for creating the v-atpase db

---
+++ V-ATPaseDB/v-atpase.sql
@@ -0,0 +1,21 @@
+/* A relational V-ATPase database of VATPase
+*  orthologs, mutations, and mutant phenotypes
+*
+* Authors: Hieu Ngyuen and Glen Ernstrom
+* Middlebury College
+* 2020-03-15
+*/
+
+
+CREATE TABLE IF NOT EXISTS "Sectors" (
+	`sector_id`	INTEGER,
+	`sector_name`	TEXT UNIQUE,
+	PRIMARY KEY(`sector_id`)
+);
+CREATE TABLE IF NOT EXISTS "Subunits" (
+	`subunit_id`	INTEGER,
+	`subunit_name`	TEXT UNIQUE,
+	`sector_id`	INTEGER,
+	PRIMARY KEY(`subunit_id`)
+);
+
